9a2b083ae43d6b08fb05b7f3f829f7a031a18083
Cole <colethrailkill8@gmail.com>
2022-06-01 23:41:09 -0400

    1.8: unicafe step 3

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,35 +1,5 @@
 import { useState } from "react";
 
-const Button = ({ type, handler}) => {
-    return <button onClick={handler}>{type}</button>;
-};
-
-const Feedback = ({ type, value }) => {
-    return <p>{type} {value}</p>;
-};
-
-const Total = ({ total }) => {
-    return <p>all {total}</p>;
-};
-
-const Average = ({ good, bad, total }) => {
-    const calculate = () => {
-        if (total === 0) {
-            return 0;
-        }
-        return (good - bad) / total;
-    };
-
-    return <p>average {calculate()}</p>
-};
-
-const Positive = ({ good, total }) => {
-    if (total === 0) {
-        return <p>positive 0%</p>
-    }
-    return <p>positive {(good / total) * 100}%</p>
-}
-
 const App = () => {
     const [statistics, setStatistics] = useState({
         good: 0, neutral: 0, bad: 0, total: 0
@@ -41,13 +11,33 @@ const App = () => {
             <Button type="good" handler={() => setStatistics({...statistics, good: statistics.good + 1, total: statistics.total + 1})} />
             <Button type="neutral" handler={() => setStatistics({...statistics, neutral: statistics.neutral + 1, total: statistics.total + 1})} />
             <Button type="bad" handler={() => setStatistics({...statistics, bad: statistics.bad + 1, total: statistics.total + 1})} />
+            <Statistics stats={statistics} />
+        </div>
+    );
+};
+
+const Button = ({ type, handler}) => {
+    return <button onClick={handler}>{type}</button>;
+};
+
+const Statistics = ({ stats }) => {
+    const average = () => {
+        return (stats.good - stats.bad) / stats.total;
+    };
+
+    const positive = () => {
+        return (stats.good / stats.total) * 100;
+    }
+    
+    return (
+        <div>
             <h1>Statistics</h1>
-            <Feedback type="good" value={statistics.good} />
-            <Feedback type="neutral" value={statistics.neutral} />
-            <Feedback type="bad" value={statistics.bad} />
-            <Total total={statistics.total} />
-            <Average good={statistics.good} bad={statistics.bad} total={statistics.total} />
-            <Positive good={statistics.good} total={statistics.total} />
+            <p>good {stats.good}</p>
+            <p>neutral {stats.neutral}</p>
+            <p>bad {stats.bad}</p>
+            <p>all {stats.total}</p>
+            <p>average {average()}</p>
+            <p>positive {positive()}%</p>
         </div>
     );
 };
