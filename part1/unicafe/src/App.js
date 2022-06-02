7c7e4bfc6750274b1bfe0e4e41eae30700413d16
Cole <colethrailkill8@gmail.com>
2022-06-01 22:01:51 -0400

    1.6 updated

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -13,33 +13,12 @@ const App = () => {
         good: 0, neutral: 0, bad: 0
     });
 
-    const incrementGood = () => {
-        setStatistics({
-            ...statistics,
-            good: statistics.good + 1
-        });
-    };
-
-    const incrementNeutral = () => {
-        setStatistics({
-            ...statistics,
-            neutral: statistics.neutral + 1
-        });
-    };
-
-    const incrementBad = () => {
-        setStatistics({
-            ...statistics,
-            bad: statistics.bad + 1
-        });
-    };
-
     return (
         <div>
             <h1>Give Feedback</h1>
-            <Button type="good" handler={incrementGood} />
-            <Button type="neutral" handler={incrementNeutral} />
-            <Button type="bad" handler={incrementBad} />
+            <Button type="good" handler={() => setStatistics({...statistics, good: statistics.good + 1})} />
+            <Button type="neutral" handler={() => setStatistics({...statistics, neutral: statistics.neutral + 1})} />
+            <Button type="bad" handler={() => setStatistics({...statistics, bad: statistics.bad + 1})} />
             <h1>Statistics</h1>
             <Feedback type="good" value={statistics.good} />
             <Feedback type="neutral" value={statistics.neutral} />
