35ef5d4f27a3eb286008a237673146ee4f761e70
Cole <colethrailkill8@gmail.com>
2022-06-07 02:21:23 -0400

    2.7: phonebook part 2

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,48 @@
+import { useState } from "react";
+
+const App = () => {
+    const [persons, setPersons] = useState([
+        { name: "Arto Hellas" }
+    ]);
+    const [newName, setNewName] = useState("");
+
+    const addPerson = (event) => {
+        event.preventDefault();
+
+        if (persons.filter((person) => person.name === newName).length > 0) {
+            alert(`${newName} is already added to phonebook`);
+            return;
+        }
+
+        const person = {
+            name: newName
+        };
+
+        setPersons(persons.concat(person));
+        setNewName("");
+    };
+
+    const handleNameChange = (event) => {
+        setNewName(event.target.value);
+    };
+
+    return (
+        <div>
+            <h2>Phonebook</h2>
+            <form onSubmit={addPerson}>
+                <div>
+                    name: <input value={newName} onChange={handleNameChange} />
+                </div>
+                <div>
+                    <button type="submit">add</button>
+                </div>
+            </form>
+            <h2>Numbers</h2>
+            {
+                persons.map((person) => <p key={person.name}>{person.name}</p>)
+            }
+        </div>
+    );
+};
+
+export default App;
